8221a453cffb263bdf57de9faa228e4eb9bff241
unknown <csesujon155@gmail.com>
2024-09-20 13:58:07 -0700

    RILT Ready StarterPack

---
+++ README.md
@@ -0,0 +1,14 @@
+<p align="center">
+    <a href="https://github.com/codersujon" target="_blank">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjZ6H-4MmOECB1h-zPYnqCIt6RvFrOJav5Vw&s"
+            width="150" alt="Laravel Logo">
+    </a>
+</p>
+
+
+## RILT Starter Pack 2024
+
+### Premium Partners
+
+- **[Vehikl](https://vehikl.com/)**
+
---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () =>{
+    return (
+        <div class="flex flex-col h-screen items-center justify-center">
+            <img src="https://miro.medium.com/v2/resize:fit:522/0*Hdm7hBTZ-hKlbtlV.png" alt="React Logo"  width="250"/>
+            <h1 class="text-5xl md:text-7xl text-center py-6 text-slate-900">
+                RILT Starter Pack 2024
+            </h1>
+            <caption class="text-3xl text-slate-700">Created_At: 20 Sep, 2024</caption>
+        </div>
+    );
+}
+
+export default Home;
